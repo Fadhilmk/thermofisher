9e775c17d81d43997d7c0df9746ccf31d83174e7
Asrith Rajeevan <asrithrajeevan@Asriths-MacBook-Air.local>
2024-07-13 00:39:01 +0530

    Revert "feedback changes"
    This reverts commit fc847ec2fc7a5e842e0b8dc47008b922a39f3041.

--- components/contact.jsx
+++ components/contact.jsx
@@ -3,40 +3,39 @@ import Image from 'next/image';
 
 const ContactUs = () => {
   return (
-    <div id='contact' className="relative">
-      {/* <div className="absolute inset-0">
+    <div id='contact' className="relative bg-gray-100">
+      <div className="absolute inset-0">
         <Image
           src="/icons/contact.png"
           alt="Background Image"
           layout="fill"
           objectFit="cover"
           quality={100}
-          className="opacity-50"
+          className="opacity-30"
         />
-      </div> */}
-      <div className="relative mx-auto px-4 py-5">
-      <h1 className="text-4xl text-center text-blue-600 font-bold mb-4">CONTACT US</h1>
-        <div className="flex flex-col lg:flex-row items-center lg:items-start p-8 rounded-lg">
-          <div className="resp-contact w-full lg:w-1/2 mb-8 lg:mb-0 text-center justify-center">
-            <p className="mb-8 font-bold ">
+      </div>
+      <div className="relative mx-auto px-4 py-12">
+        <div className="flex flex-col lg:flex-row items-center lg:items-start  p-8 rounded-lg">
+          <div className="w-full lg:w-1/2 mb-8 lg:mb-0">
+            <h1 className="text-3xl text-center font-bold mb-4">Contact Us</h1>
+            <p className="mb-4">
               We&aposd love to hear from you! Please fill out the form on the right to get in touch with us.
             </p>
-            <p className="mb-8">
+            <p className="mb-4">
               Address: 1234 Street Name, City, State, ZIP
             </p>
-            <p className="mb-8">
+            <p className="mb-4">
               Phone: (123) 456-7890
             </p>
             <p>
               Email: contact@example.com
             </p>
           </div>
-          <div className="w-full lg:w-1/2 lg:p-8 shadow-lg border rounded border-gray-300">
-            <form className="space-y-4 p-6">
+          <div className="w-full lg:w-1/2 lg:p-8 shadow-lg border border-gray-300">
+            <form className="space-y-4">
               <div>
                 <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label>
                 <input
-                  required
                   type="text"
                   id="name"
                   name="name"
@@ -46,7 +45,6 @@ const ContactUs = () => {
               <div>
                 <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
                 <input
-                  required
                   type="email"
                   id="email"
                   name="email"
@@ -56,7 +54,6 @@ const ContactUs = () => {
               <div>
                 <label htmlFor="message" className="block text-sm font-medium text-gray-700">Message</label>
                 <textarea
-                  required
                   id="message"
                   name="message"
                   rows="4"
@@ -66,7 +63,7 @@ const ContactUs = () => {
               <div>
                 <button
                   type="submit"
-                  className="w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  className="w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                   Send
                 </button>
